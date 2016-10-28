e65fabe1de840fc17ccd9f57bbb94d94d6fbf40f
michael-melville <michael.melville@ryerson.ca>
2016-10-28 15:36:17 -0400

    Timestepping tweaks.

--- StructDyn_TEST.m
+++ StructDyn_TEST.m
@@ -1,9 +1,8 @@
 clear,clc
 
-valDELTIME = 0.00001;
 valTIMESTEP = 0;
-valNSELE = 20;
-vecLIFT = 500*ones(1,valNSELE); 
+valNSELE = 200;
+vecLIFT = 5000*ones(1,valNSELE); 
 vecMOM = zeros(1,valNSELE);
 matDEFLECTION = zeros(1,valNSELE+4);
 matTWIST = zeros(1,valNSELE+4);
@@ -14,13 +13,13 @@ vecSPANINERTIA_2 = zeros(1,valNSELE);
 matSPANINERTIA = [vecSPANINERTIA; vecSPANINERTIA_1; vecSPANINERTIA_2];
 vecLINMASS = 2700*vecSPANAREA; 
 vecTORSIONRIGIDITY = 27e9*14583.3333333333*(1/1000)^4*ones(1,valNSELE);
-vecMASS2SHEAR = 0*ones(1,valNSELE);
+vecMASS2SHEAR = 0.1*ones(1,valNSELE);
 valYMODULUS = 69e9; 
 valSPAN = 5;
 
 valTOL = 100;
 
-valINITCOND = 0.001;
+valINITCOND = 0.0;
 
 % Preallocate space for deflection and twist vectors
 vecDEFLECTION = zeros(1,valNSELE+4);
@@ -32,23 +31,34 @@ valDY = valSPAN/(valNSELE-1);
 % Vector of beam spanwise locations where deflection and twist is computed
 vecBEAM = 0:valDY:valSPAN;
 
-while valTOL > 0.00005
+valDELTIME = sqrt(150000*valDY^4/valYMODULUS);
+
+% while valTOL > 0.00005
+
+for valTIMESTEP = 1:5000
     
-    valTIMESTEP = valTIMESTEP + 1;
+    % valTIMESTEP = valTIMESTEP + 1;
 
     [vecDEFLECTION, vecTWIST, vecBEAM] = fcnWINGTWISTBEND(valDELTIME, valTIMESTEP, vecLIFT, vecMOM, matDEFLECTION, matTWIST, vecSPANAREA, matSPANINERTIA, vecLINMASS, vecTORSIONRIGIDITY, vecMASS2SHEAR, valYMODULUS, valNSELE, valSPAN, vecDEFLECTION, vecTWIST, vecBEAM, valINITCOND, valDY);
     
     matDEFLECTION(valTIMESTEP,:) = vecDEFLECTION;
     matTWIST(valTIMESTEP,:) = vecTWIST;
 
-    if valTIMESTEP > 2
-	    valTOL = abs((matDEFLECTION(valTIMESTEP,valNSELE+2)-matDEFLECTION(valTIMESTEP-1,valNSELE+2))/...
-	    	matDEFLECTION(valTIMESTEP-1,valNSELE+2))*100;
-    end
+    % if valTIMESTEP > 2
+	   %  valTOL = abs((matDEFLECTION(valTIMESTEP,valNSELE+2)-matDEFLECTION(valTIMESTEP-1,valNSELE+2))/...
+	   %  	matDEFLECTION(valTIMESTEP-1,valNSELE+2))*100;
+    % end
     
 end
 
+figure(1)
 plot(vecBEAM(:),matDEFLECTION(valTIMESTEP,3:valNSELE+2))
 xlabel('Span (m)')
 ylabel('Deflection (m)')
 title('Wing Spanwise Deflection')
+
+figure(2)
+plot(vecBEAM(:),(180/(2*pi)).*matTWIST(valTIMESTEP,3:valNSELE+2))
+xlabel('Span (m)')
+ylabel('Twist (deg)')
+title('Wing Spanwise Twist')
