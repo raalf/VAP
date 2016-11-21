3cc85fe2480cc00deaef8dffc83a75fbb601673b
devinbarcelos <devinbarcelos@gmail.com>
2016-11-21 16:56:07 -0500

    Geometry rotation for multiple blades REFERENCE ONLY

--- fcnDVEMULTIROTOR.m
+++ fcnDVEMULTIROTOR.m
@@ -1,17 +1,50 @@
 function [valNELE, matNEWNPVLST, vecAIRFOIL, vecDVELE, vecDVETE, vecDVEYAW, vecDVEPANEL, vecDVETIP, vecDVEWING, vecDVESYM, vecM, vecN, vecDVEROLL, vecDVEAREA, vecDVEPITCH, vecDVEMCSWP, vecDVETESWP, vecDVELESWP, vecDVEHVCRD, vecDVEHVSPN, vecSYM, matADJE, matNEWCENTER, matNEWVLST, matDVE, matNEWDVENORM] = fcnDVEMULTIROTOR(valNELE, valNUMB, vecDVETIP, vecDVETESWP, vecDVEPITCH, vecDVEWING, vecDVEMCSWP, vecM, vecN, vecDVEPANEL, vecDVEROLL, vecDVELESWP, vecDVEYAW, vecDVEHVCRD, vecDVEHVSPN, vecDVEAREA, vecDVESYM, vecDVELE, vecDVETE, vecSYM, vecROTAX, vecAIRFOIL, matNPVLST, matDVE, matADJE, matVLST, matCENTER, matDVENORM)
-%   This function modifies the created DVEs and all required input values
-%       for multiple rotor blades.
+% This function modifies the created DVEs and all required input values
+%	for multiple rotor blades.
 %
-%   Inputs:
+% Inputs:
+%   Parameters from input file and fcnGENERATEDVES that require
+%   modification
 %
-%   Outputs
+% Outputs:
+%   Each input has been modified for the given number of blades:
+%   From input file
+%       valAREA - multiply by valNUMB
+%       vecSYM - Add (will be deleted)
+%       vecAIRFOIL - add
+%       vecN - add
+%       venM - add
 %
+%   From Generate DVEs
+%		matCENTER - rotate values center values
+%		vecDVEHVSPN - add to vector for each blade
+%		vecDVEHVCRD - add to vector for each blade
+%		vecDVELESWP - add to vector for each blade
+%		vecDVEMCSWP - add to vector for each blade
+%		vecDVETESWP - add to vector for each blade
+%		vecDVEROLL - add to vector for each blade
+%		vecDVEPITCH - add to vector for each blade
+%		vecDVEYAW - add to vector for each blade
+%		vecDVEAREA - add to vector for each blade
+%		matDVENORM - rotate values normal vectors
+%		matVLST - rotate vertices
+%		valNELE - multiply by valNUMB
+%		matDVE - matDVE + max(matDVE) for each blade
+%		matADJE - matADJE + max(matADJE) for each blade
+%		vecDVESYM - delete in later versions
+%		vecDVETIP - add to vector for each blade
+%		vecDVEWING - vecDVEWING + max(vecDVEWING) for each blade
+%		vecDVELE - add to vector for each blade
+%		vecDVETE - add to vector for each blade
+%		vecDVEPANEL - vecDVEPANEL + max(vecDVEPANEL) for each blade
 
+
+%% Create rotation matrix
 % Angle of each blade from original (+ve CCW & in rad)
 tempTHETA = (0:2*pi/valNUMB:2*pi)';
 tempTHETA(valNUMB+1) = [];
 
-%% Create rotation matrix
+% Develop 
 tempTOP = [cos(tempTHETA), -sin(tempTHETA), zeros([valNUMB,1])];
 tempMID = [sin(tempTHETA), cos(tempTHETA), zeros([valNUMB,1])];
 tempLOW = [zeros([valNUMB, 1]), zeros([valNUMB, 1]), ones([valNUMB, 1])];
@@ -19,7 +52,9 @@ tempROTATE = reshape(tempTOP',[1, 3, valNUMB]);
 tempROTATE(2,:,:) = reshape(tempMID',[1, 3, valNUMB]);
 tempROTATE(3,:,:) = reshape(tempLOW',[1, 3, valNUMB]);
 
+% 2D rotation matrix
 tempROTATE2D = (reshape(permute(tempROTATE,[2,1,3]),[3 valNUMB*3]))';
+
 
 %% Parameters the must Rotate
 % Make Each point relative to rotation axis
@@ -44,9 +79,9 @@ matNEWVLST = reshape(permute(temp,[2,1,3]),[3,valNUMB*(numel(tempVLST)/3)])' + v
 temp = reshape(tempNEWNPVLST,[numel(tempNPVLST)/3,3,valNUMB]);
 matNEWNPVLST = reshape(permute(temp,[2,1,3]),[3,valNUMB*(numel(tempNPVLST)/3)])' + vecROTAX;
 
-
 temp = reshape(tempDVENORM,[numel(matDVENORM)/3,3,valNUMB]);
 matNEWDVENORM = reshape(permute(temp,[2,1,3]),[3,valNUMB*(numel(matDVENORM)/3)])';
+
 
 %% Parameters to increase vector for number of blades
 vecAIRFOIL = repmat(vecAIRFOIL,[valNUMB,1]);
@@ -91,7 +126,7 @@ tempADDI = (reshape(repmat(1:valNUMB,numel(vecDVEPANEL),1),[valNUMB*numel(vecDVE
 vecDVEPANEL = repmat(vecDVEPANEL,[valNUMB 1])+tempADDI;
 
 
-%% Plot for make sure 
+%% Scatter plot of centers and verticies for validation
 % figure(1)
 % clf(1)
 % hold on
@@ -106,8 +141,6 @@ vecDVEPANEL = repmat(vecDVEPANEL,[valNUMB 1])+tempADDI;
 % axis equal
 % axis tight
 % hold off
-
-
 
 end
 
