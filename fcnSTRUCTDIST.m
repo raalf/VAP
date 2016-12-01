0f7451cc5d7ae00c21df679a26c9ab2c168ce6e4
michael-melville <michael.melville@ryerson.ca>
2016-12-01 17:47:36 -0500

    Added structure input file and reading functions.

---
+++ fcnSTRUCTDIST.m
@@ -0,0 +1,33 @@
+function [matEIx, matGJt, vecEA, vecCG, vecLM, vecLSM] = fcnSTRUCTDIST(vecN,valSPAN,valPANELS,vecDVEHVSPN,vecDVELE,vecEIxCOEFF,vecGJtCOEFF,vecEACOEFF,vecCGCOEFF,vecLMCOEFF)
+
+% Find DVEs on LE of wing
+[ledves, ~, ~] = find(vecDVELE > 0);
+
+% Determine spanwise location of DVEs
+tempSPANDIST = 2*vecDVEHVSPN(ledves);
+matSPANDIST = repmat(tempSPANDIST,1,size(tempSPANDIST));
+
+temp = triu(matSPANDIST);
+
+vecSPANDIST = sum(temp(:,1:size(matSPANDIST,2)))';
+vecSPANDIST = [0; vecSPANDIST];
+
+% Spanwise bending stiffness distribution. Cols 2 and 3 are the first and
+% second derivatives
+matEIx(:,1) = (vecEIxCOEFF(1).*vecSPANDIST.^2 + vecEIxCOEFF(2).*vecSPANDIST + vecEIxCOEFF(3))';
+matEIx(:,2) = (2*vecEIxCOEFF(1).*vecSPANDIST + vecEIxCOEFF(2))';
+matEIx(:,3) = (repmat(2*vecEIxCOEFF(1),1,size(vecSPANDIST)))';
+
+% Spanwise torsional stiffness distribution. Col 2 is the first derivative
+matGJt(:,1) = (vecGJtCOEFF(1).*vecSPANDIST.^2 + vecGJtCOEFF(2).*vecSPANDIST + vecGJtCOEFF(3))';
+matGJt(:,2) = (2*vecGJtCOEFF(1).*vecSPANDIST + vecGJtCOEFF(2))';
+
+vecEA = vecEACOEFF(1).*vecSPANDIST.^2 + vecEACOEFF(2).*vecSPANDIST + vecEACOEFF(3);
+vecCG = vecCGCOEFF(1).*vecSPANDIST.^2 + vecCGCOEFF(2).*vecSPANDIST + vecCGCOEFF(3);
+vecLM = vecLMCOEFF(1).*vecSPANDIST.^2 + vecLMCOEFF(2).*vecSPANDIST + vecLMCOEFF(3);
+
+vecLSM = vecEA - vecCG;
+
+end
+
+
---
+++ fcnSTRUCTREAD.m
@@ -0,0 +1,82 @@
+function [vecEIxCOEFF, vecGJtCOEFF, vecEACOEFF, vecCGCOEFF, vecLMCOEFF] = fcnSTRUCTREAD(strSTRUCT_INPUT)
+
+% This function reads in the structure input file and stores each parameter
+% in a vector. The vectors are all 1 x 3 with col 1 = A, col 2 = B, and col
+% 3 = C, where A, B, and C make up the function y  = A*x^2 + B*x + C
+%
+% INPUT
+% strSTRUCT_INPUT - String pointing to path of structure input file
+%
+% OUTPUT
+% vec_EIx - 1 x 3 vector containing A, B, and C coefficients of quadratic
+% bending stiffness distribution
+% vec_GJt - 1 x 3 vector containing A, B, and C coefficients of quadratic
+% torsional stiffness distribution
+% vec_EA - 1 x 3 vector containing A, B, and C coefficients of quadratic
+% elastic axis location distribution
+% vec_CG - 1 x 3 vector containing A, B, and C coefficients of quadratic
+% center of gravity location distribution
+% vec_LM - 1 x 3 vector containing A, B, and C coefficients of quadratic
+% linear mass distribution
+
+% Preallocate variables
+vecEIxCOEFF = zeros(1,3);
+vecGJtCOEFF = zeros(1,3);
+vecEACOEFF = zeros(1,3);
+vecCGCOEFF = zeros(1,3);
+vecLMCOEFF = zeros(1,3);
+
+% Open structure input file
+fp = fopen(strSTRUCT_INPUT);
+
+% Read A,B,C values for bending stiffness distribution
+for i = 1:3
+    ch = fscanf(fp,'%c',1);
+    while(ch~='=')
+        ch = fscanf(fp,'%c',1);
+    end
+    vecEIxCOEFF(i) = fscanf(fp,'%lf',1);
+
+end
+
+% Read A,B,C values for torsional stiffness distribution
+for i = 1:3
+    ch = fscanf(fp,'%c',1);
+    while(ch~='=')
+        ch = fscanf(fp,'%c',1);
+    end
+    vecGJtCOEFF(i) = fscanf(fp,'%lf',1);
+
+end
+
+% Read A,B,C values for elastic axis distribution
+for i = 1:3
+    ch = fscanf(fp,'%c',1);
+    while(ch~='=')
+        ch = fscanf(fp,'%c',1);
+    end
+    vecEACOEFF(i) = fscanf(fp,'%lf',1);
+
+end
+
+% Read A,B,C values for center of gravity distribution
+for i = 1:3
+    ch = fscanf(fp,'%c',1);
+    while(ch~='=')
+        ch = fscanf(fp,'%c',1);
+    end
+    vecCGCOEFF(i) = fscanf(fp,'%lf',1);
+
+end
+
+% Read A,B,C values for linear mass distribution
+for i = 1:3
+    ch = fscanf(fp,'%c',1);
+    while(ch~='=')
+        ch = fscanf(fp,'%c',1);
+    end
+    vecLMCOEFF(i) = fscanf(fp,'%lf',1);
+
+end
+
+fclose(fp);
