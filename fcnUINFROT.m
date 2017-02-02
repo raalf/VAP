e5ef03dd79a41cfb7100605e850feccc2dea8dd8
devinbarcelos <devinbarcelos@gmail.com>
2017-02-02 16:12:16 -0500

    Fixed multiple rotor velocity calculations

--- fcnUINFROT.m
+++ fcnUINFROT.m
@@ -1,4 +1,4 @@
-function [matUINF] = fcnUINFROT(matCENTER, vecROTAX, valTIMESTEP, valRPM, valALPHAR, valAZNUM, valDIA, valJ)
+function [matUINF] = fcnUINFROT(matCENTER, vecROTAX, valTIMESTEP, valRPM, valALPHAR, valAZNUM, valDIA, valJ, valNUMB)
 
 % This function defines the direction of the inflow velocity
 
@@ -16,21 +16,31 @@ function [matUINF] = fcnUINFROT(matCENTER, vecROTAX, valTIMESTEP, valRPM, valALP
 % Finding the rotation angle for the timestep
 valDELROT = (2*pi)/valAZNUM;
 
+% Number of DVEs per blade
+tempTOTDVE = size(matCENTER);
+tempNUMDVE = tempTOTDVE(1)/valNUMB;
+
 % Find the current rotor angle (relative to initial position)
-tempTHETA = valDELROT*valTIMESTEP;
+temp = 0:2*pi/(valNUMB):2*pi;
+temp(valNUMB+1) = [];
+vecTHETA = valDELROT*valTIMESTEP+reshape(repmat(temp,tempNUMDVE,1),tempTOTDVE(1),1);
+
 
 % Convert rpm to rad per second
 tempRADPS = valRPM*2*pi/60;
 
 % Radial points of control points from rotational axis
-tempCENTER = matCENTER - repmat(vecROTAX, length(matCENTER(:,1)),1);
+tempCENTER = matCENTER - repmat(vecROTAX, tempTOTDVE(1),1);
 
-% Radius Magnidute
+% Radius Magnitude
 tempRMAG = sqrt(tempCENTER(:,1).^2+tempCENTER(:,2).^2+tempCENTER(:,3).^2);
 
-vecUROT = tempRADPS*tempRMAG*[cos(tempTHETA) sin(tempTHETA) 0];
+matUROT = tempRADPS*[tempRMAG.*cos(vecTHETA) tempRMAG.*sin(vecTHETA) zeros(tempTOTDVE(1),1)];
 vecUTRANS = (valJ*valDIA*valRPM/60)*[cos(valALPHAR) 0 sin(valALPHAR)];
 
-matUINF = vecUROT + vecUTRANS;
+matUINF = matUROT - vecUTRANS;
+
+
+%quiver3(matCENTER(:,1),matCENTER(:,2),matCENTER(:,3),matUINF(:,1),matUINF(:,2),matUINF(:,3))
 end
 
