27b0fa70d3873ae72a5ee6a14bed78b9b218eda6
devinbarcelos <devinbarcelos@gmail.com>
2017-02-09 15:47:46 -0500

    Adjusted element force calculations for rotors (freestream and induced)

--- fcnRFORCES.m
+++ fcnRFORCES.m
@@ -2,7 +2,10 @@ function [nind, nfree] = fcnRFORCES( valWSIZE, valTIMESTEP, valNELE, valWNELE, s
 % Force function where each force contributation is calculated and added.
 
 %% Element normal forces, lift forces and side forces (freestream and induced)
-[nind, nfree] = fcnRDVENFORCE(valWSIZE, valTIMESTEP, valNELE, valWNELE, seqALPHAR, vecDVEPITCH, vecK, vecWK, vecWDVEYAW, vecWDVELESWP, vecWDVETESWP, vecDVEYAW, vecDVEMCSWP, vecWDVEHVSPN, vecWDVEHVCRD, vecWDVEROLL, vecDVEROLL,  vecDVEHVCRD, vecDVELE, vecDVEHVSPN, vecWDVEPITCH, vecDVELESWP, vecDVETESWP, vecSYM, matVLST, matDVE, matUINF, matCOEFF, matADJE, matWDVE, matWVLST, matCENTER, matWCOEFF);
+[nind, nfree, thrustind, thrustfree, sideind, sidefree, axialind, axialfree] = fcnRDVENFORCE(valWSIZE, valTIMESTEP, valNELE, valWNELE, seqALPHAR, vecDVEPITCH, vecK, vecWK, vecWDVEYAW, vecWDVELESWP, vecWDVETESWP, vecDVEYAW, vecDVEMCSWP, vecWDVEHVSPN, vecWDVEHVCRD, vecWDVEROLL, vecDVEROLL,  vecDVEHVCRD, vecDVELE, vecDVEHVSPN, vecWDVEPITCH, vecDVELESWP, vecDVETESWP, vecSYM, matVLST, matDVE, matUINF, matCOEFF, matADJE, matWDVE, matWVLST, matCENTER, matWCOEFF);
+
+%% Calculate induced drag on the rotor
+
 
 end
 
