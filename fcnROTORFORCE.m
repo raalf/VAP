992cf11be7af3ab366384d30de6f973f4d6a12da
devinbarcelos <devinbarcelos@gmail.com>
2017-03-09 12:36:44 -0500

    Begin to correct torque calculation. Still must fix axial induced and axial freestream torque contributions

--- fcnROTORFORCE.m
+++ fcnROTORFORCE.m
@@ -1,4 +1,4 @@
-function [valCT, valCQ, vecCTCONV] = fcnROTORFORCE(thrustind, thrustfree, axialind, axialfree, inddrag, valRPM, valDIA, valAZNUM, valTIMESTEP, vecCTCONV)
+function [valCT, valCQ, vecCTCONV] = fcnROTORFORCE(thrustind, thrustfree, thrustCFfree, axialind, axialfree, inddrag, valRPM, valDIA, valAZNUM, valTIMESTEP, vecCTCONV, vecCPRADI)
 %   This function uses the previously calculated induced and freestream
 %   forces and calculates non-dimensionalized values.
 %
@@ -10,8 +10,8 @@ function [valCT, valCQ, vecCTCONV] = fcnROTORFORCE(thrustind, thrustfree, axiali
 %   vecCTCONV - Convergence thrust, vector the length of valAZNUM
 
 % Calculate total force values per density
-thrust = sum(thrustind)+sum(thrustfree);
-torque = sum(axialind)+sum(axialfree)+sum(inddrag);
+thrust = sum(thrustind)+sum(thrustfree)+sum(thrustCFfree);
+torque = sum(axialind.*vecCPRADI+axialfree.*vecCPRADI+inddrag.*vecCPRADI);
 
 % Calculate non-dimensionalized coefficient using US customary definitions
 % CT = T/(rho*Omega^2*D^4)
