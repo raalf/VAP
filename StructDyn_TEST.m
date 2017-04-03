28f0ddf5663bceec3682b561d103f11f08026bb6
michael-melville <michael.melville@ryerson.ca>
2017-04-03 09:36:05 -0400

    Modified test file to model free vibraiton

--- StructDyn_TEST.m
+++ StructDyn_TEST.m
@@ -1,126 +1,137 @@
+clear,clc
 
-valSPAN = valSPAN/2;
+valNSELE = 25;
 
-valINITCOND = 0.0;
+valDY = 5/valNSELE;
 
-valDY = sum(2*vecDVEHVSPN,1)/length(vecDVEHVSPN);
+valSTRUCTDELTIME = 0.0001;
 
-valNSELE = sum(vecN,1)+1;
+matEIx(:,1) = 20000*ones(valNSELE,1);
+matEIx(:,2) = zeros(valNSELE,1);
+matEIx(:,3) = zeros(valNSELE,1);
 
-valSTRUCTDELTIME = valDELTIME;
+matGJt(:,1) = 10000*ones(valNSELE,1);
+matGJt(:,2) = zeros(valNSELE,1);
 
-C = -0.0333*vecSPANDIST + 0.76*ones(1,length(vecSPANDIST)) ;
-tk = 0.02 ;
-Tk = 0.15 ;
-vecSPANAREA = pi*tk*C*(1 + Tk);
+vecJT = 0.1*ones(valNSELE,1);
 
-vecTWIST = zeros(1,valNSELE+4);
+vecLM = 5*ones(valNSELE,1);
+vecLSM = 0*ones(valNSELE,1);
 
-for valTIMESTEP = 3:3500
-    
-valSTRUCTTIME = valTIMESTEP;
+vecDEF = zeros(1,valNSELE+3);
+vecTWIST = zeros(1,valNSELE+3);
+vecSLOPE = zeros(1,valNSELE-1);
 
-if valTIMESTEP >= 3
+vecLIFTDIST = zeros(1,valNSELE);
+vecMOMDIST = zeros(1,valNSELE);
 
-    matDEF(1:valSTRUCTTIME-1,:) = matDEFGLOB(1:valTIMESTEP-1,:);
+%% Beam boundary conditions
 
-    matTWIST(1:valSTRUCTTIME-1,:) = matTWISTGLOB(1:valTIMESTEP-1,:);
+matDEF(1:2,:) = zeros(2,valNSELE+3);
+matTWIST(1:2,:) = zeros(2,valNSELE+3);
 
-    vecDEF(3) = 0; % Zero deflection at root BC
-    vecTWIST(3) = 0; % Zero twist at root BC
+for valSTRUCTTIME = 3:10000
 
-    % Assemble load matrix
-    matLOAD = [vecLIFTDIST' - vecLM.*9.81, vecMOMDIST - vecLM.*vecLSM.*9.81];
+vecDEF(2) = 0; % Zero deflection at root BC
+vecTWIST(2) = 0; % Zero twist at root BC
 
-    for yy = 4:(valNSELE+2)
+% Assemble load matrix
+matLOAD = [vecLIFTDIST' - vecLM.*9.81, vecMOMDIST' - vecLM.*vecLSM.*9.81];
+% matLOAD(end,:) = [0,0]; 
 
-        %% Geometric property assembly
+for yy = 3:(valNSELE+1)
 
-        % Assemble mass matrix
-        matMASS = [vecLM(yy-2), -vecLM(yy-2).*vecLSM(yy-2); -vecLM(yy-2).*vecLSM(yy-2), vecLM(yy-2).*(vecLSM(yy-2)+(vecJT(yy-2)./(vecSPANAREA(yy-2))))];
+    %% Geometric property assembly
 
-        % Assemble stiffness matrices
-        matK_1 = [matEIx(yy-2,3), 0; 0, 0];
-        matK_2 = [matEIx(yy-2,2), 0; 0, -matGJt(yy-2,2)]; 
-        matK_3 = [matEIx(yy-2,1), 0; 0, -matGJt(yy-2,1)];
-        matB = [0 0; 0 10000];
+    % Assemble mass matrix
+%             matMASS = [vecLM(yy-2), -vecLM(yy-2).*vecLSM(yy-2); -vecLM(yy-2).*vecLSM(yy-2), vecLM(yy-2)*(vecLSM(yy-2)*vecLSM(yy-2) + vecJT(yy-2)/vecSPANAREA(yy-2))];
+    matMASS = [vecLM(yy-1), -vecLM(yy-1).*vecLSM(yy-1); -vecLM(yy-1).*vecLSM(yy-1), vecJT(yy-1)];
 
-        %% Finite difference relations for partial derivatives
+    % Assemble stiffness matrices
+    matK_1 = [matEIx(yy-1,3), 0; 0, 0];
+    matK_2 = [matEIx(yy-1,2), 0; 0, -matGJt(yy-1,2)]; 
+    matK_3 = [matEIx(yy-1,1), 0; 0, -matGJt(yy-1,1)];
+    matB = [60 0; 0 0];
 
-        % Finite difference relations for partial derivatives w.r.t
-        % time
-        valUDOT = (matDEF(valSTRUCTTIME-1,yy) - matDEF(valSTRUCTTIME - 2, yy))./valSTRUCTDELTIME;
-        valTDOT = (matTWIST(valSTRUCTTIME-1,yy) - matTWIST(valSTRUCTTIME - 2,yy))./valSTRUCTDELTIME; 
+    %% Finite difference relations for partial derivatives
 
-        % Finite difference relations for partial derivative of deflection w.r.t Y
-        valU_yy = (matDEF(valSTRUCTTIME-1,yy+1) - 2*matDEF(valSTRUCTTIME-1,yy) + matDEF(valSTRUCTTIME-1,yy-1))/(valDY)^2;
-        valU_yyy = (matDEF(valSTRUCTTIME-1,yy+2) - 3*matDEF(valSTRUCTTIME-1,yy+1) + 3*matDEF(valSTRUCTTIME-1,yy)- ...
-            matDEF(valSTRUCTTIME-1,yy-1))/(valDY)^3;
-        valU_yyyy = (matDEF(valSTRUCTTIME-1,yy+2) - 4*matDEF(valSTRUCTTIME-1,yy+1) + 6*matDEF(valSTRUCTTIME-1,yy) - ...
-            4*matDEF(valSTRUCTTIME-1,yy-1) + matDEF(valSTRUCTTIME-1,yy-2))/(valDY)^4;
-
-        % Finite difference relations for partial derivative of twist w.r.t Y
-        valTHETA_y = (matTWIST(valSTRUCTTIME-1,yy+1) - matTWIST(valSTRUCTTIME-1,yy-1))/2*valDY;
-        valTHETA_yy = (matTWIST(valSTRUCTTIME-1,yy+1) - 2*matTWIST(valSTRUCTTIME-1,yy) + matTWIST(valSTRUCTTIME-1,yy-1))/(valDY^2);
-
-        %% Solve matrix equation
-
-        % Temp variable with the wing deflection and twist stored as a matrix. The
-        % first row is the deflection, w/ each column as a spanwise station. The
-        % second row is the twist, w/ each column as a spanwise station.
-
-        tempTWISTBEND = 2.*[matDEF(valSTRUCTTIME-1,yy); matTWIST(valSTRUCTTIME-1,yy)] - [matDEF(valSTRUCTTIME-2,yy); matTWIST(valSTRUCTTIME-2,yy)] ...
-            + (valSTRUCTDELTIME^2).*inv(matMASS)*([matLOAD(yy-2,1); matLOAD(yy-2,2)] - matK_1*[valU_yy; 0] - matK_2*[valU_yyy; valTHETA_y] -...
-            matK_3*[valU_yyyy; valTHETA_yy] - matB*[valUDOT; valTDOT]);
-
-        % Output result of deflection and twist to separate vectors
-        vecDEF(yy) = tempTWISTBEND(1,:);
-        vecTWIST(yy) = tempTWISTBEND(2,:);
-
-        % Calculate angle between DVE and horizontal based on
-        % deflection
-        vecSLOPE(yy-3) = asin((vecDEF(yy)-vecDEF(yy-1))/(vecSPANDIST(yy-2)-vecSPANDIST(yy-3)));
-
-    end
+    % Finite difference relations for partial derivatives w.r.t
+    % time
+    valUDOT = (matDEF(valSTRUCTTIME-1,yy) - matDEF(valSTRUCTTIME - 2, yy))./valSTRUCTDELTIME;
+    valTDOT = (matTWIST(valSTRUCTTIME-1,yy) - matTWIST(valSTRUCTTIME - 2,yy))./valSTRUCTDELTIME;
         
-vecDEF(valNSELE+3) = 2*vecDEF(valNSELE+2)...
-    -vecDEF(valNSELE+1); % BC for deflection one element beyond wing (positive span direction)
+    % Finite difference relations for partial derivative of deflection w.r.t Y
+    valU_yy = (matDEF(valSTRUCTTIME-1,yy+1) - 2*matDEF(valSTRUCTTIME-1,yy) + matDEF(valSTRUCTTIME-1,yy-1))/(valDY)^2;
+    valU_yyy = (matDEF(valSTRUCTTIME-1,yy+2) - 3*matDEF(valSTRUCTTIME-1,yy+1) + 3*matDEF(valSTRUCTTIME-1,yy)- ...
+        matDEF(valSTRUCTTIME-1,yy-1))/(valDY)^3;
+    valU_yyyy = (matDEF(valSTRUCTTIME-1,yy+2) - 4*matDEF(valSTRUCTTIME-1,yy+1) + 6*matDEF(valSTRUCTTIME-1,yy) - ...
+        4*matDEF(valSTRUCTTIME-1,yy-1) + matDEF(valSTRUCTTIME-1,yy-2))/(valDY)^4;
 
-vecDEF(valNSELE+4) = 3*vecDEF(valNSELE+2)...
-    -2*vecDEF(valNSELE+1); % BC for deflection two elements beyond wing (positive span direction)
+    % Finite difference relations for partial derivative of twist w.r.t Y
+    valTHETA_y = (matTWIST(valSTRUCTTIME-1,yy+1) - matTWIST(valSTRUCTTIME-1,yy-1))/(2*valDY);
+    valTHETA_yy = (matTWIST(valSTRUCTTIME-1,yy+1) - 2*matTWIST(valSTRUCTTIME-1,yy) + matTWIST(valSTRUCTTIME-1,yy-1))/(valDY^2);
 
-vecDEF(2) = vecDEF(4); % BC for deflection one element beyond root (negative span direction)
+    %% Solve matrix equation
 
-vecTWIST(valNSELE+3) = vecTWIST(valNSELE+1); % BC for twist one element beyond wing tip (positive span direction)
+    % Temp variable with the wing deflection and twist stored as a matrix. The
+    % first row is the deflection, w/ each column as a spanwise station. The
+    % second row is the twist, w/ each column as a spanwise station.
+
+    tempTWISTBEND = 2.*[matDEF(valSTRUCTTIME-1,yy); matTWIST(valSTRUCTTIME-1,yy)] - [matDEF(valSTRUCTTIME-2,yy); matTWIST(valSTRUCTTIME-2,yy)] ...
+        + (valSTRUCTDELTIME^2).*inv(matMASS)*([matLOAD(yy-1,1); matLOAD(yy-1,2)] - matK_1*[valU_yy; 0] - matK_2*[valU_yyy; valTHETA_y] -...
+        matK_3*[valU_yyyy; valTHETA_yy] - matB*[valUDOT; valTDOT]);
+
+    % Output result of deflection and twist to separate vectors
+    vecDEF(yy) = tempTWISTBEND(1,:);
+    vecTWIST(yy) = tempTWISTBEND(2,:);
+
+    % Calculate angle between DVE and horizontal based on
+    % deflection
+    vecSLOPE(yy-2) = asin((vecDEF(yy)-vecDEF(yy-1))/(valDY));
+
+end
+
+vecDEF(valNSELE+2) = 2*vecDEF(valNSELE+1)...
+    -vecDEF(valNSELE); % BC for deflection one element beyond wing (positive span direction)
+
+vecDEF(valNSELE+3) = 3*vecDEF(valNSELE+1)...
+    -2*vecDEF(valNSELE); % BC for deflection two elements beyond wing (positive span direction)
+
+vecDEF(1) = vecDEF(3); % BC for deflection one element beyond root (negative span direction)
+
+vecTWIST(valNSELE+2) = vecTWIST(valNSELE); % BC for twist one element beyond wing tip (positive span direction)
 
 matDEF(valSTRUCTTIME,:) = vecDEF;
 matTWIST(valSTRUCTTIME,:) = vecTWIST;
-
-end
 
 % Spanwise deflection and twist wrt structural timestep
 vecDEF = matDEF(end,:);
 vecTWIST = matTWIST(end,:);
 
-% Spanwise deflection and twist wrt to global timestep
-
-if valTIMESTEP >= 3
-    
-    matDEFGLOB(valTIMESTEP,:) = matDEF(end,:);
-    matTWISTGLOB(valTIMESTEP,:) = matTWIST(end,:);
-
-    matSLOPE(valTIMESTEP,:) = vecSLOPE';
-
-else
-    
-    matDEFGLOB(valTIMESTEP,:) = matDEF(end,:);
-    matTWISTGLOB(valTIMESTEP,:) = matTWIST(end,:);
-
-    matSLOPE(valTIMESTEP,:) = vecSLOPE;
-
 end
 
+valNELE = size(vecLIFTDIST,2);
+
+valDY = 0.01;
+valNELE = 500;
+
+vecLOAD = -5*ones(1,valNELE)*9.81;
+matEIx = [];
+matEIx(:,1) = 20000*ones(valNELE,1);
+
+S(valNELE) = 0;
+M(valNELE) = 0;
+
+for yy = (valNELE-1):-1:1
+    S(yy) = S(yy+1) - ((vecLOAD(yy+1)+vecLOAD(yy))/2)*valDY;
+    M(yy) = M(yy+1) - ((S(yy+1)+S(yy))/2)*valDY;
 end
 
-% end
+theta(1) = 0;
+w(1) = 0;
 
+for yy = 2:valNELE
+    theta(yy) = theta(yy-1) + 0.5*((M(yy)+M(yy-1))/matEIx(yy,1))*valDY;
+    w(yy) = w(yy-1) + ((theta(yy)+theta(yy-1))/2)*valDY;
+    vecSLOPE(yy) = asin((w(yy)-w(yy-1))/(valDY));
+end
--- fcnSTATICTWIST.m
+++ fcnSTATICTWIST.m
@@ -2,78 +2,85 @@ function [theta_dist] = fcnSTATICTWIST(vecLIFTDIST, vecDVEHVSPN, matGJt, vecEDGE
     valTIMESTEP, valDENSITY, valUINF, valSPAN, vecLEDVES, matCENTER, vecSPANDIST, vecDVELE, vecMOMDIST)
 
 s = 0.5*valSPAN;
-% 
-% matGJt = 20000*(1-vecSPANDIST/(2*s)).^2;
-% [ledves, ~, ~] = find(vecDVELE > 0);
-% 
-% tempSPANDIST = matCENTER(ledves,2);
-% 
-% tempSPANDIST = repmat(tempSPANDIST', size(tempSPANDIST,1),1);
-% 
-% tempSPANDIST = triu(tempSPANDIST);
-% 
-% vecCLDIST = repmat(vecCLDIST', size(vecCLDIST,1),1);
-% 
-% vecCLDIST = triu(vecCLDIST);
-% 
-% vecCLDIST = ((vecSPANDIST(2:(end-1))' - tempSPANDIST(1,1:(end-1)))./(tempSPANDIST(2,2:end)-...
-%     tempSPANDIST(1,1:(end-1)))).*(vecCLDIST(2,2:end)-vecCLDIST(1,1:(end-1))) + vecCLDIST(1,1:(end-1)); % Linear interpolation
-% 
-% CL_root = vecCLDIST(1,1) - (tempSPANDIST(1,1) - vecSPANDIST(1)).*(vecCLDIST(1,2)-vecCLDIST(1,1))./(tempSPANDIST(1,2)-tempSPANDIST(1,1));
-% vecCLDIST = [CL_root, vecCLDIST, 0];
-% 
-% % vecCL_alpha = vecCLDIST./(vecEDGEPITCH+matTWISTGLOB(valTIMESTEP-1,:));
-% vecCL_alpha = (0.07*(1-vecSPANDIST./s).^2)./sqrt(1-0.5*0.5);
-% 
-% % q = 0.5*valDENSITY*valUINF*valUINF;
+e = 0.25;
+c = 1;
+
+% vecSPANDIST = 0:0.1:8;
+eta = vecSPANDIST./s;
+
+% matGJt = 20000*(1-eta/2).^2;
+[ledves, ~, ~] = find(vecDVELE > 0);
+
+tempSPANDIST = matCENTER(ledves,2);
+
+tempSPANDIST = repmat(tempSPANDIST', size(tempSPANDIST,1),1);
+
+tempSPANDIST = triu(tempSPANDIST);
+
+vecCLDIST = repmat(vecCLDIST', size(vecCLDIST,1),1);
+
+vecCLDIST = triu(vecCLDIST);
+
+vecCLDIST = ((vecSPANDIST(2:(end-1))' - tempSPANDIST(1,1:(end-1)))./(tempSPANDIST(2,2:end)-...
+    tempSPANDIST(1,1:(end-1)))).*(vecCLDIST(2,2:end)-vecCLDIST(1,1:(end-1))) + vecCLDIST(1,1:(end-1)); % Linear interpolation
+
+CL_root = vecCLDIST(1,1) - (tempSPANDIST(1,1) - vecSPANDIST(1)).*(vecCLDIST(1,2)-vecCLDIST(1,1))./(tempSPANDIST(1,2)-tempSPANDIST(1,1));
+vecCLDIST = [CL_root, vecCLDIST, 0];
+
+vecCL_alpha = vecCLDIST./(vecEDGEPITCH+matTWISTGLOB(valTIMESTEP-1,:));
+% vecCL_alpha = (0.07*(1-eta).^2)./sqrt(1-0.5*0.5);
+
+q = 0.5*valDENSITY*valUINF*valUINF;
 % q = 0.5*1.4*50000*0.5*0.5;
-% 
-% % f = -0.0000809.*(vecSPANDIST).^2 + 0.0028988.*(vecSPANDIST);
-% f = vecSPANDIST./s;
-% 
-% % f_prime = 2*-0.0000809.*(vecSPANDIST) + 0.0028988;
-% f_prime = 1/s;
-% 
-% A = (1/s).*trapz(linspace(0,s,18),matGJt(:,1).*f_prime.*f_prime);
-% 
-% % vecLIFT = vecLIFTDIST(1:(end-1))'.*(2*vecDVEHVSPN(vecLEDVES));
-% % vecLIFT = [vecLIFT; 0];
-% 
-% B = ((1.5*1.5)*0.1/s).*trapz(linspace(0,s,18),vecCL_alpha.*f.*f);
-% 
-% C = ((1.5*1.5)*0.1/s).*trapz(linspace(0,s,18),(vecCL_alpha*5.*f));
-% 
-% theta_r = q*C/(A - B);
-% 
-% qd = A/(s*s*B);
-% 
-% theta_dist = (theta_r.*f);
 
-coeff = coeffvalues(fit(vecSPANDIST,vecMOMDIST,'poly9'));
+% f = (-0.0000809*(s*s)).*(eta).^2 + (0.0028988*s).*(eta);
+f = sin(0.5*pi*eta);
+% f = eta;
 
-n = 1;
-for i = 10:-1:1
-    
-    temp_int(i) = coeff(n)*s^(i)/(i);
-    coeff_new(n) = coeff(n)/i;
-    n = n + 1;
-    
-end
+% f_prime = 2*(-0.0000809*(s*s)).*(eta) + 0.0028988*s;
+f_prime = 0.5*pi*cos(0.5*pi*eta);
+% f_prime = 1;
 
-C1 = sum(temp_int);
+A = trapz(linspace(0,1,size(vecSPANDIST,1)),matGJt(:,1).*f_prime.*f_prime);
 
-coeff_new = [coeff_new, C1];
+vecLIFT = vecLIFTDIST(1:(end-1))'.*(2*vecDVEHVSPN(vecLEDVES));
+vecLIFT = [vecLIFT; 0];
 
-coeff_new = coeff_new./((11:-1:1)*matGJt(1,1));
+B = ((c*c)*e).*trapz(linspace(0,1,size(vecSPANDIST,1)),vecCL_alpha'.*f.*f);
 
-n = 1;
-for i = 11:-1:1
-    
-    temp_theta(n,:) = coeff_new(n)*vecSPANDIST'.^(i);
-    n = n + 1;
-    
-end
+C = ((c*c)*e).*trapz(linspace(0,1,size(vecSPANDIST,1)),(vecLIFT.*f));
 
-theta_dist = sum(temp_theta,1);
+theta_r = q*s*s*C/(A - q*s*s*B);
+
+qd = A/(s*s*B);
+
+theta_dist = (theta_r.*f);
+
+% coeff = coeffvalues(fit(vecSPANDIST,vecMOMDIST,'poly9'));
+% 
+% n = 1;
+% for i = 10:-1:1
+%     
+%     temp_int(i) = coeff(n)*s^(i)/(i);
+%     coeff_new(n) = coeff(n)/i;
+%     n = n + 1;
+%     
+% end
+% 
+% C1 = sum(temp_int);
+% 
+% coeff_new = [coeff_new, C1];
+% 
+% coeff_new = coeff_new./((11:-1:1)*matGJt(1,1));
+% 
+% n = 1;
+% for i = 11:-1:1
+%     
+%     temp_theta(n,:) = coeff_new(n)*vecSPANDIST'.^(i);
+%     n = n + 1;
+%     
+% end
+% 
+% theta_dist = sum(temp_theta,1);
 
 end
